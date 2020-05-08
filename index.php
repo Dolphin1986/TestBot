81bce9962f7e61e0ce4524df402f65e992570205
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-08 11:59:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$update = json_decode(file_get_contents('php://input'));
+$update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
 
 echo "Привет это update - ". $update;
 
