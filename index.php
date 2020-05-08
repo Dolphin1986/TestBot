a6bd14fc427aa72d5cbb7dc6b2cfe99675337f75
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-08 12:59:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,44 +4,19 @@ include 'webhook.php';
 
 print_r($update);
 
-
 ?>
 
-<!doctype html>
+
+<!DOCTYPE html>
 <html>
 <head>
-<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+	<title>Test Slider</title>
 </head>
-<body>
+	
+	<body>
+			<h1>Тут должна быть картинка:</h1>
+		<img src = '<?=$update['img_url01']?>'>
 
-	<img src='<? echo($update['img_url01'])?>' id="img_url">
+	</body>
 
-<script type="text/javascript">
-$(document).ready(function() {
-    $('#img_url01').submit(function(e) {
-        e.preventDefault();
-        $.ajax({
-            type: "GET",
-            url: 'webhook.php',
-            data: $(this).serialize(),
-            success: function(response)
-            {
-                var jsonData = JSON.parse(response);
- 
-                // user is logged in successfully in the back-end
-                // let's redirect
-                if (jsonData.success == "1")
-                {
-                    location.href = 'my_profile.php';
-                }
-                else
-                {
-                    alert('Invalid Credentials!');
-                }
-           }
-       });
-     });
-});
-</script>
-</body>
 </html>
