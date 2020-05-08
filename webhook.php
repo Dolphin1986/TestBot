2bfc63076dffd5b2b9aa72fd39fc4731eb7e0f0f
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-08 15:27:56 +0300

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -7,7 +7,14 @@ $update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
 
 file_put_contents(__DIR__.'/log.txt', file_get_contents('php://input'));
 
-var_dump($update);
+session_start(); 
+$_SESSION['value1'] = $update; 
+
+
+
+var_dump($_SESSION['value1']);
+
+ // ETC... echo "<input type='text' name='value1' value='".$_SESSION['value1']."' />"; echo "<input type='text' name='value2' value='".$_SESSION['value2']."' />"; 
 
 
 // $url    = 'php://input';
