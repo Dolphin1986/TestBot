987de42f82f5aafea5af8e9ba0706a2c1b140571
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-08 12:09:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ $update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
 
 print_r($update);
 
-echo "Привет это update - ". $update;
+echo "Привет это update - ". $update['img_url01'];
 
