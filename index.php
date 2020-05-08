ff7ba505e8560de71e98ef49acb5302667a25233
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-08 12:54:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,16 +7,41 @@ print_r($update);
 
 ?>
 
-
-<!DOCTYPE html>
+<!doctype html>
 <html>
 <head>
-	<title>Test Slider</title>
+<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 </head>
-	
-	<body>
+<body>
 
-		<img src = '<?=$update['img_url01']?>'>
-	</body>
+	<img src='<? echo($update['img_url01'])?>' id="img_url">
 
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#img_url01').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "GET",
+            url: 'webhook.php',
+            data: $(this).serialize(),
+            success: function(response)
+            {
+                var jsonData = JSON.parse(response);
+ 
+                // user is logged in successfully in the back-end
+                // let's redirect
+                if (jsonData.success == "1")
+                {
+                    location.href = 'my_profile.php';
+                }
+                else
+                {
+                    alert('Invalid Credentials!');
+                }
+           }
+       });
+     });
+});
+</script>
+</body>
 </html>
