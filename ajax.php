e75fdcc97fcec40904a467748cde8badfbbb5487
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-08 15:21:40 +0300

    05

--- ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-$update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
-
-if (isset($update))
-{
-   echo $update['txt_massage'];
-}
-die();
- 
--- index.php
+++ index.php
@@ -1,42 +1,24 @@
-<!doctype html>
+<?php
+
+include 'webhook.php';
+
+print_r($update);
+
+?>
+
+
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Пример ajax</title>
+	<title>Test Slider</title>
 </head>
-<body>
-<p>Здесь появится случайное число</p>
-<button>Старт</button>
-<div id="loader" style="display:none;"><img src="loader.gif"></div>
-<script src="js/jquery.js"></script>
-<script>
-$("button").click(function()
-{
-   $.ajax({
-      // метод передачи данных POST
-      type: "POST",
-      // на сервере обратимся к файлу ajax.php,
-      // который находится рядом с index.php
-      url: "ajax.php",
-      // передаем две переменные с именами min и max, 
-      // равные 1 и 100 соответственно
-      data: {min: 1, max: 100},
-      // перед отправкой показываем loader.gif
-      beforeSend: function() {$('#loader').fadeIn();},
-      // если получен ответ от сервера, то скрываем loader.gif за 300 мс
-      // и помещаем в <p></p> полученное число
-      success: function(res) {
-          $('#loader').fadeOut(300, function() {$("p").text(res);});
-      },
-      // если ошибка, то скрываем loader.gif за 300 мс и 
-      // затем помещаем в <p></p> текст о недоступности сервера
-      error: function() {
-          $('#loader').fadeOut(300, 
-             function() {$("p").text('Сервер временно недоступен');});
-      }
-      });
-   return false;
-});
-</script>
-</body>
+	
+	<body>
+		<h2>Текст сообщения вот: <?=$update['txt_massage']?></h2>
+		<br>
+		<h1>Тут должна быть картинка:</h1>
+		<img src = '<?=$update['img_url01']?>'>
+		<a href="/log.txt">log.txt</a>
+	</body>
+
 </html>
