29b6cf123c393bf77e518e43f6d7f080b17e6932
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-08 12:15:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,15 @@ $update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
 print_r($update);
 
 echo "Привет это update - ". $update['img_url01'];
+?>
 
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test Slider</title>
+</head>
+<body>
+<img src="<?php echo $update['img_url01'];?>">
+</body>
+</html>
