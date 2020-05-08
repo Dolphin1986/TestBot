7c1a29025e41851dda6ede5e33e00ab54d9337a7
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-08 13:13:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
-include 'webhook.php';
+// include 'webhook.php';
+
+$update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
+
+file_put_contents(__DIR__.'/log.txt', file_get_contents('php://input'));
 
 var_dump($update);
 
@@ -14,9 +18,9 @@ var_dump($update);
 </head>
 	
 	<body>
-			<h1>Тут должна быть картинка:</h1>
+		<h1>Тут должна быть картинка:</h1>
 		<img src = '<?=$update['img_url01']?>'>
-
+		<a href="/log.txt">log.txt</a>
 	</body>
 
 </html>
