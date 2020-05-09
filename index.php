a4d5ecda3f8b85f0f0b4d80f041c3d2e7e1cdda7
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-09 11:42:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'webhook.php';
 $last_log = './last_log.txt';
 $log = './log.txt';
 
-$hook = file_get_contents($last_log, true));
+$hook = file_get_contents($last_log, true);
 
 echo "Это Hook - ".$hook.'<br>';
 
