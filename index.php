44761f9d092542941379666c919d43c0f7e0b9cf
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-10 11:41:11 +0300

    04

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'webhook.php';
 $last_log = './last_log.txt';
 $log = './log.txt';
 
-$hook = file_get_contents($lasr_log, true);
+$hook = file_get_contents($lasr_log);
 
 echo "Это Hook - ".$hook.'<br>';
 
